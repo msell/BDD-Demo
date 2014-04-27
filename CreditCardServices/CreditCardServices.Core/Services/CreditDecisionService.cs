3fda66600b0d5269c2a822b13ca992e39cacecfd
Matt Sell <matt.sell@improvingenterprises.com>
2014-04-27 14:52:49 -0500

    create second test

--- CreditCardServices/CreditCardServices.Core/Services/CreditDecisionService.cs
+++ CreditCardServices/CreditCardServices.Core/Services/CreditDecisionService.cs
@@ -4,9 +4,21 @@ namespace CreditCardServices.Core.Services
 {
     public class CreditDecisionService
     {
+        ICreditReportService _creditService;
+
+        public CreditDecisionService(ICreditReportService creditService)
+        {
+            _creditService = creditService;
+        }
         public DecisionResponse GetDecision(DecisionRequest decisionRequest)
         {
-            return new DecisionResponse {Result = DecisionResult.Approved};
+            var creditReport = _creditService.CheckCreditHistory(new CreditReportRequest());
+            var decision = new DecisionResponse {Result = DecisionResult.Declined};
+
+            if(creditReport.CreditScore > 600)
+                decision.Result = DecisionResult.Approved;
+
+            return decision;
         }
     }
 }
--- CreditCardServices/CreditCardServices.Tests/DecisionService/DescribeCreditDecisionService.cs
+++ CreditCardServices/CreditCardServices.Tests/DecisionService/DescribeCreditDecisionService.cs
@@ -2,6 +2,8 @@
 using CreditCardServices.Core.Services;
 using Machine.Specifications;
 using FluentAssertions;
+using Moq;
+using It = Machine.Specifications.It;
 
 namespace CreditCardServices.Tests.DecisionService
 {
@@ -10,13 +12,40 @@ namespace CreditCardServices.Tests.DecisionService
     {
         Establish context = () =>
         {
-            sut = new CreditDecisionService();
+            var fakeCreditService = Mock.Of<ICreditReportService>();
             
+            Mock.Get(fakeCreditService)
+                .Setup(x => x.CheckCreditHistory(Moq.It.IsAny<CreditReportRequest>()))
+                .Returns(new CreditReport {CreditScore = 720});
+
+            sut = new CreditDecisionService(fakeCreditService);
         };
         Because of = () => response = sut.GetDecision(decisionRequest);
 
         It should_approve_the_applicant = () => response.Result.Should().Be(DecisionResult.Approved);
 
+        static CreditDecisionService sut;
+        static DecisionResponse response;
+        static DecisionRequest decisionRequest;
+    }
+
+    class when_the_applicant_has_poor_credit
+    {
+        Establish context = () =>
+        {
+            var fakeCreditService = Mock.Of<ICreditReportService>();
+
+            Mock.Get(fakeCreditService)
+                .Setup(x => x.CheckCreditHistory(Moq.It.IsAny<CreditReportRequest>()))
+                .Returns(new CreditReport { CreditScore = 400 });
+
+            sut = new CreditDecisionService(fakeCreditService);
+        };
+        
+        Because of = () => response = sut.GetDecision(decisionRequest);
+        
+        It should_decline_the_applicant = () => response.Result.Should().Be(DecisionResult.Declined);
+        
         static CreditDecisionService sut;
         static DecisionResponse response;
         static DecisionRequest decisionRequest;
