70cabcbda728c887fc6aeeedcc397bc7a3963cf0
Matt Sell <matt.sell@improvingenterprises.com>
2014-04-26 22:24:41 -0500

    fake service bus

---
+++ CreditCardServices/CreditCardServices.Api/Services/ServiceBus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CreditCardServices.Api.Services
+{
+    public class ServiceBus : IBus
+    {
+        
+    }
+
+    public interface IBus
+    {
+
+    }
+}
