613cba40ca7094452a1d7172d6293f0005b14e02
Matt Sell <matt.sell@improvingenterprises.com>
2014-04-27 18:49:38 -0500

    introduce nbuilder

--- CreditCardServices/CreditCardServices.Tests/DecisionService/DescribeCreditDecisionService.cs
+++ CreditCardServices/CreditCardServices.Tests/DecisionService/DescribeCreditDecisionService.cs
@@ -1,5 +1,6 @@
 ﻿using CreditCardServices.Core.Models;
 using CreditCardServices.Core.Services;
+using FizzWare.NBuilder;
 using Machine.Fakes;
 using Machine.Specifications;
 using FluentAssertions;
@@ -11,12 +12,11 @@ namespace CreditCardServices.Tests.DecisionService
     class when_the_applicant_has_excellent_credit : WithSubject<CreditDecisionService>
     {
         Establish context = () =>
-        {
-            The<ICreditReportService>().WhenToldTo(x=>x
-                .CheckCreditHistory(Param.IsAny<CreditReportRequest>()))
-                .Return(new CreditReport{CreditScore = 720});
-        };
-       
+            The<ICreditReportService>().WhenToldTo(x => x
+            .CheckCreditHistory(Param.IsAny<CreditReportRequest>()))
+                .Return(Builder<CreditReport>.CreateNew()
+                    .With(x => x.CreditScore = CreditRating.Excellent.LowerBoundary).Build);
+
         Because of = () => response = Subject.GetDecision(decisionRequest);
 
         It should_approve_the_applicant = () => response.Result.Should().Be(DecisionResult.Approved);
@@ -28,16 +28,17 @@ namespace CreditCardServices.Tests.DecisionService
     class when_the_applicant_has_poor_credit : WithSubject<CreditDecisionService>
     {
         Establish context = () =>
-        {
+
             The<ICreditReportService>().WhenToldTo(x => x
-                .CheckCreditHistory(Param.IsAny<CreditReportRequest>()))
-                .Return(new CreditReport { CreditScore = 400 });
-        };
-        
+            .CheckCreditHistory(Param.IsAny<CreditReportRequest>()))
+                .Return(Builder<CreditReport>.CreateNew()
+                    .With(x => x.CreditScore = CreditRating.Poor.LowerBoundary).Build);
+
+
         Because of = () => response = Subject.GetDecision(decisionRequest);
-        
+
         It should_decline_the_applicant = () => response.Result.Should().Be(DecisionResult.Declined);
-        
+
         static DecisionResponse response;
         static DecisionRequest decisionRequest;
     }
