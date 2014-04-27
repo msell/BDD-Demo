ba1c84f67b353303200084e97a5e699b04effe4e
Matt Sell <matt.sell@improvingenterprises.com>
2014-04-27 10:19:51 -0500

    add first failing test

---
+++ CreditCardServices/CreditCardServices.Tests/DecisionService/DescribeCreditDecisionService.cs
@@ -0,0 +1,43 @@
+﻿using Machine.Specifications;
+using FluentAssertions;
+
+namespace CreditCardServices.Tests.DecisionService
+{
+    class when_the_applicant_has_excellent_credit
+    {
+        Establish context = () =>
+        {
+            sut = new CreditDecisionService();
+            
+        };
+        Because of = () => response = sut.GetDecision(decisionRequest);
+
+        It should_approve_the_applicant = () => response.Should().Be(DecisionResult.Approved);
+
+        static CreditDecisionService sut;
+        static DecisionResponse response;
+        static DecisionRequest decisionRequest;
+    }
+
+    internal class CreditDecisionService
+    {
+        public DecisionResponse GetDecision(DecisionRequest decisionRequest)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    internal class DecisionRequest
+    {
+    }
+
+    internal class DecisionResponse
+    {
+        DecisionResult Result { get; set; }
+    }
+
+    internal enum DecisionResult
+    {
+        Error, Approved, Declined
+    }
+}
